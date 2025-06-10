0621e55250016292503efa56654acab18fe04bcd
Queentel <joiqueentel@gmail.com>
2025-06-10 10:21:14 +0300

    creates aithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1; // Always > 0
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4; 
+
+const max = Math.max(5, 10, 15, 20);
+
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max
+};
